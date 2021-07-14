bea6af1fc9fec758eec0279b46713c651086447f
Rizwa <rizwanzaheer405@gmail.com>
2021-07-14 12:59:32 +0200

    handled cart and checkout page

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
                 'carts.context_processors.counter',
-                
+
             ],
         },
     },
@@ -133,3 +133,16 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'rizwanzaheer405@gmail.com'
+EMAIL_HOST_PASSWORD = 'Rizwan312751773cb'
+EMAIL_USE_TLS = True
